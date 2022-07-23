73630d538288b01a3da8b6d9f7d2a7a42b9cdc99
Cristhian-rodrigues <100093372+Cristhiannnn@users.noreply.github.com>
2022-07-22 22:49:54 -0300

    desenvolvendo

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,10 @@
 
 function openNav(){
-    window.alert("testando")
+    let mob = document.querySelector('.mob'); /* manipulando o menu mobile através do DOM*/
+
+    if (mob.classList.contains('open')) {
+        mob.classList.remove('open');
+    } else {
+        mob.classList.add ('open');
+    } /* estrutura condicional para verificar , remover ou adicioanar a classe (.open) */
 } 
